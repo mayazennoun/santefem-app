cb94d166eee23e081f8ac6398c2ad13cb978c72f
Maya Zennoun <zennounmaya@gmail.com>
2025-12-30 14:25:39 +0100

    Add Firebase configuration details

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -3,6 +3,12 @@ import { Auth, getAuth } from 'firebase/auth';
 import { Firestore, getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
+  apiKey: 'AIzaSyBe2258L1JWAeclL-Htmx_zTzbAqkMm_UE',
+  authDomain: 'santefem-9ae74.firebaseapp.com',
+  projectId: 'santefem-9ae74',
+  storageBucket: 'santefem-9ae74.appspot.com',
+  messagingSenderId: '590326820096',
+  appId: '1:590326820096:web:2094e70c7e192da2b1d7e5',
 };
 
 const app = initializeApp(firebaseConfig);
